a6c2d4f8ff942c3e535098a1164d80602318fede
Hamam-S <h.sousou97@gmail.com>
2024-02-25 05:04:16 +0100

    Camera : input with glfw

--- ConsoleApplication1/ConsoleApplication1/Window.h
+++ ConsoleApplication1/ConsoleApplication1/Window.h
@@ -19,6 +19,10 @@ public:
 
 	bool getShouldClose() { return glfwWindowShouldClose(mainWindow); }
 
+	bool* getKeys() { return keys; }
+	GLfloat getXChange();
+	GLfloat getYChange();
+
 	void swapBuffers() { glfwSwapBuffers(mainWindow); }
 
 	~Window();
@@ -28,4 +32,14 @@ private:
 
 	GLint width, height;
 	GLint bufferWidth, bufferHeight;
+
+	bool keys[1024];
+
+	GLfloat lastX, lastY, xChange = 0.0f, yChange = 0.0f;
+	bool mouseFirstMoved;
+
+	void createCallbacks();
+	static void handleKeys(GLFWwindow* window, int key, int code, int action, int mode);
+
+	static void handleMouse(GLFWwindow* window, double xPos, double yPos);
 };
