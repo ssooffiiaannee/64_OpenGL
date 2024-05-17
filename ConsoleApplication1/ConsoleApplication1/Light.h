a55741ef2c25037a02b8b08356f490595cdd461d
Hamam-S <h.sousou97@gmail.com>
2024-05-17 19:18:55 +0200

    add light and material source files

---
+++ ConsoleApplication1/ConsoleApplication1/Light.h
@@ -0,0 +1,22 @@
+#pragma once
+
+#include <GL\glew.h>
+#include <glm\glm.hpp>
+
+class Light
+{
+public:
+	Light();
+	Light(GLfloat red, GLfloat green, GLfloat blue, GLfloat aIntensity, GLfloat xDir, GLfloat yDir, GLfloat zDir, GLfloat dIntensity);
+
+	void UseLight(GLuint ambientIntensityLocation, GLuint ambientColourLocation, GLuint diffuseIntensityLocation, GLuint directionLocation);
+
+	~Light();
+
+private:
+	glm::vec3 colour;
+	GLfloat ambientIntensity;
+
+	glm::vec3 direction;
+	GLfloat diffuseIntensity;
+};
