9b38d29012d450f6212f4d4ba7ea917aa31547d9
Hamam-S <h.sousou97@gmail.com>
2024-06-09 15:15:34 +0200

    add omp test

--- ConsoleApplication1/ConsoleApplication1/Source.cpp
+++ ConsoleApplication1/ConsoleApplication1/Source.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include "schrodinger.h"
 #include <complex>
+#include <omp.h>
 using namespace std;
 
 schrodinger s(2, 1, 1, 0);
@@ -8,15 +9,23 @@ schrodinger s(2, 1, 1, 0);
 
 int main()
 {
-	printf("hello world\n");
-	printf("%f\n", s.laguerrePoly(2, 1, 6));
-	assert(s.laguerrePoly(2, 1, 6) == 3.0, "First wrong !");
-	
+	//printf("hello world\n");
+	//printf("%f\n", s.laguerrePoly(2, 1, 6));
+	//assert(s.laguerrePoly(2, 1, 6) == 3.0, "First wrong !");
+	//
 
-	auto res = s.sphericalHarmonics(2, 1, PI / 2, PI / 2);
-	printf("%f %f\n", res.imag(), res.real());
+	//auto res = s.sphericalHarmonics(2, 1, PI / 2, PI / 2);
+	//printf("%f %f\n", res.imag(), res.real());
 
-	res = s.sphericalHarmonics(3, 1, PI / 2, PI / 3);
-	printf("%f %f\n", res.imag(), res.real());
+	//res = s.sphericalHarmonics(3, 1, PI / 2, PI / 3);
+	//printf("%f %f\n", res.imag(), res.real());
+
+	omp_set_num_threads(10);
+#pragma omp parallel
+	{
+		int id = omp_get_thread_num();
+
+		printf("%d %d %d\n", id, omp_get_num_threads(), omp_get_max_threads());
+	}
 	return 0;
 }
